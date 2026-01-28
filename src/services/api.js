2e03f1b94626fbf9125b6bc6b5520b99f11f748e
NayenRC <nay.roman@duocuc.cl>
2026-01-28 17:29:11 -0300

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { supabase } from "./supabaseClient";
 
+console.log("🔥 VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
