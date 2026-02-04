7b37ff640b265667774fa6634b82a5116b3ba465
NayenRC <nay.roman@duocuc.cl>
2026-02-04 05:51:26 -0300

    "auth"

--- src/services/authService.js
+++ src/services/authService.js
@@ -62,6 +62,7 @@ export async function register(email, password) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+      nombre: "Usuario",
       email: email,
       password: password,
     }),
